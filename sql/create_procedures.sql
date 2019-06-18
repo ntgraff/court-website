a485f64e99697bcc554f4d31c8c0129bce0248cf
George Hosea <georgewilliamhosea@gmail.com>
2019-06-18 00:53:47 -0400

    Added functions and procedures

--- sql/create_procedures.sql
+++ sql/create_procedures.sql
@@ -56,10 +56,54 @@ BEGIN
 	FROM parties
 	WHERE party_id = pid;
 END ;;
--- A function that checks if a login was successful (username, password) -> boolean
--- A function that checks if a user can register (username) -> boolean
+
+
+-- A function that checks if a login was successful (username, password) -> TINYINT(1 IS TRUE, 0 IS FALSE)
+drop function if exists succesful_login;
+
+DELIMITER //
+
+CREATE FUNCTION succesful_login(un varchar(255), pw varchar(255)) RETURNS TINYINT(1) DETERMINISTIC
+BEGIN
+	DECLARE num int;
+    SELECT count(*) into num FROM users WHERE username = un and password = pw;
+    return encounter;
+END //
+
+DELIMITER ;
+
+
+-- A function that checks if a user can register (username) -> TINYINT(1 IS TRUE, 0 IS FALSE)
+drop function if exists available_username;
+
+DELIMITER //
+CREATE FUNCTION available_username(un varchar(255)) RETURNS TINYINT(1) DETERMINISTIC
+BEGIN
+	DECLARE num int;
+    SELECT count(*) into num FROM users WHERE username = un;
+    if num = 1 then 
+		return 0; 
+    END IF;
+    return 1;
+END //
+
+DELIMITER ;
+
 -- A procedure to register a user (username, password) => modify users table
+DROP PROCEDURE IF EXISTS  add_user;
+
+DELIMITER ;;
+CREATE PROCEDURE add_user(us VARCHAR(255), pw VARCHAR(255))
+BEGIN
+	INSERT INTO users(username, password) VALUES (us, pw);
+END ;;
+
+
 -- One of those trigger things that removes a reservation when it is over maybe?
+-- Maybe instead a function that deletes reservations for a specific time period
+-- This way it can keep old records in case someone wants to check something
+-- and easily delete old records when needed
+-- idk
 --   not sure if we need this one
 -- Add cascade stuff to the tables
 -- Maybe add more constraints to the tables? not sure.
